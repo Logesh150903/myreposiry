310fb7eb0f3ba612c9c9d89c7810ee5d82769d43
Logesh150903 <logeshkumar1509@gmail.com>
2024-05-25 16:43:40 +0530

    fristcode

---
+++ demo/src/demo/example.java
@@ -0,0 +1,21 @@
+package demo;
+
+public class example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int	a,b,c,d,e,f,g;
+		a=100;
+		b=200;
+		c=a+b;
+		d=a*b;
+		e=b/a;
+		f=a%b;
+		g=b-a;
+		System.out.println("addition:"+c);
+		System.out.println("subration:"+g);
+		System.out.println("muylp:"+d);
+		System.out.println("div:"+e);
+		System.out.println("mod:"+f);
+	}
+}
